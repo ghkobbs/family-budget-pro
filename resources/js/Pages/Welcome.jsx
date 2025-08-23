f03bd10ca93f263516303290d230b542d9c1d0af
Maxwell Morrison <maxkobbs@gmail.com>
2025-08-23 21:08:33 +0000

    +

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -59,7 +59,7 @@ export default function Welcome({ auth }) {
                                             href={route('register')}
                                             className="rounded-full bg-blue-600 px-8 py-3 text-base font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 dark:bg-blue-500 dark:hover:bg-blue-400"
                                         >
-                                            Start Free Trial
+                                            Start For Free
                                         </Link>
                                         <a
                                             href="#features"
